d233670a655f0756fc0f99264c4dd18923021a8c
bomj321 <mauricio1314@hotmail.com>
2019-03-06 09:55:03 -0430

    Optimizando codigo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,42 +6,21 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <title>Ventas</title>
-
-        <!-- Fonts -->
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-<!--BOOTSTRAP-->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
-<!--BOOTSTRAP-->
-
-<!--ICONOS-->
-<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css">
-<!--ICONOS-->
-
-<!--ESTILOS PERSONALIZADOS-->
-<link href="{{ asset('css/multiselect.css') }}" rel="stylesheet" type="text/css" >
-<link href="{{ asset('css/styles.css') }}" rel="stylesheet" type="text/css" >
-
-<!--ESTILOS PERSONALIZADOS-->
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet" type="text/css" >
     </head>
 
     <body>
-      <div class="container-fluid">
+            <div class="container-fluid">
 
-@include('includes.header')
-
-
-@include('includes.body')
+                  @include('includes.header')
 
 
-@include('includes.footer')
-</div>
+                  @include('includes.body')
 
+
+                  @include('includes.footer')
+           </div>
     </body>
-
-
-
-
-
 
 
 <!--SCRIPTS PERSONALIZADOS-->
